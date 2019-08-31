da711072d74fedca74f2523d385c7d317be092d6
Renan <rgmelo94@gmail.com>
2019-08-31 11:02:58 -0300

    template inicial

---
+++ src/store/modules/user/saga.js
@@ -0,0 +1,29 @@
+import { put, call, all, takeLatest } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+import api from '~/services/api';
+
+export function* updateProfileRequest({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = Object.assign(
+      { name, email },
+      rest.oldPassword ? rest : {}
+    );
+
+    const response = yield call(api.put, 'user', profile);
+
+    yield put(updateProfileSuccess(response.data));
+
+    Alert.alert('Perfil atualizado com sucesso');
+  } catch (error) {
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([
+  takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfileRequest),
+]);
